fa730e18822d18e2fb24432d0cc6f1b4bb98e776
anndy9192 <andybmq@gmail.com>
2019-10-02 08:29:15 -0500

    Se crea proyecto de protafolio, links de redes y primeras rutas

---
+++ src/components/landingpage.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import {Grid, Cell} from 'react-mdl'
+
+class Landing extends Component {
+    render(){
+        return(
+            <div style={{width: '100%', margin: 'auto' }}>
+                <Grid className='landing-grid'>
+                    <Cell col={12}>
+                        <img
+                        src='https://media.licdn.com/dms/image/C5603AQEktt24v_qycg/profile-displayphoto-shrink_200_200/0?e=1575504000&v=beta&t=V2pN_PsDfWXyRR0xVxI-Y54zdf32hqJl3KxuklP0epU'
+                        alt= "avatar"
+                        className='avatar-img'    
+                        />
+
+                        <div className="banner-text">
+                            <h1> Front end developer </h1>
+
+                            <hr/>
+
+                            <p>HTML/CSS | JavaScript | React | NodeJS | Firebase</p>
+
+                            <div className='social-links'>
+
+                                {/*LinkedIn*/}
+                                <a href='www.linkedin.com/in/andreamagallanes' rel="noopener noreferrer" target='_blank'>
+                                <i class="fa fa-linkedin-square" aria-hidden='true'/>
+                                </a>
+
+                                {/*Github*/}
+                                <a href='https://github.com/anndy9192' rel="noopener noreferrer" target='_blank'>
+                                <i class="fa fa-github-square" aria-hidden='true'/>
+                                </a>
+
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
+
+        )
+    }
+}
+
+export default Landing
---
+++ src/components/projects.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Progects extends Component {
+    render(){
+        return(
+            <div><h1>Progects Page</h1></div>
+        )
+    }
+}
+
+export default Progects
