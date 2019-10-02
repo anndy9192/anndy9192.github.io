fd0c761facd028bd6f019d3970e5721f80d309f6
anndy9192 <andybmq@gmail.com>
2019-10-02 10:47:43 -0500

    Updates

---
+++ precache-manifest.8de170bc291802f6ab8b3b36f33fbd37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5719e2423d74706113c8b4450a360dd2",
+    "url": "/myprotafolio/index.html"
+  },
+  {
+    "revision": "25608adbca1d6821e9f8",
+    "url": "/myprotafolio/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "a948cb1a855a3c62edfb",
+    "url": "/myprotafolio/static/css/main.b12faead.chunk.css"
+  },
+  {
+    "revision": "25608adbca1d6821e9f8",
+    "url": "/myprotafolio/static/js/2.5b79cc98.chunk.js"
+  },
+  {
+    "revision": "a948cb1a855a3c62edfb",
+    "url": "/myprotafolio/static/js/main.426005f6.chunk.js"
+  },
+  {
+    "revision": "3c12b7d3fb2fa569ff22",
+    "url": "/myprotafolio/static/js/runtime-main.3f50b7df.js"
+  }
+]);
