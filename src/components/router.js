3b944dbbbc5cfac7b2e3267d9831fa0aa6709321
anndy9192 <andybmq@gmail.com>
2019-11-06 17:28:16 -0600

    The handle routes was changed and the history library was installed

--- src/components/router.js
+++ src/components/router.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import createHashHistory from 'history/createHashHistory';
 import LandingPage from './landingpage';
 import AboutMe from './aboutme';
 import Contact from './contact';
 import Projects from './projects';
 import Resume from './resume';
 
+const historyObj = createHashHistory({ basename: process.env.PUBLIC_URL });
+
 const Routes= () => (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router history={historyObj}>
         <Switch>
             <Route exact path='/' component={LandingPage} />
             <Route  exact path='/aboutme' component={AboutMe} />
@@ -15,7 +18,7 @@ const Routes= () => (
             <Route  exact path='/projects' component={Projects} />
             <Route  exact path='/resume' component={Resume} />
         </Switch>
-    </BrowserRouter>
+    </Router>
 )
 
 export default Routes;
